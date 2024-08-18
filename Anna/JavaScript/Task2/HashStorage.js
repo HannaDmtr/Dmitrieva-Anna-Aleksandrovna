7d6fb809a63d1fd2bfde45007861740d89513d44
Anna Dmitrieva <ann.dmtr92@gmail.com>
2024-08-18 09:19:13 +0300

    Coctails

---
+++ Anna/JavaScript/Task2/HashStorage.js
@@ -0,0 +1,24 @@
+class HashStorage {
+    constructor() {
+        this.storage = {};
+    }
+
+    addValue(key, value) {
+        this.storage[key] = value; 
+    }
+
+    getValue(key) {
+        return this.storage[key]; 
+    }
+    deleteValue(key) {
+        if (key in this.storage) { 
+            delete this.storage[key]; 
+            return true; 
+        }
+        return false; 
+    }
+
+    getKeys() {
+        return Object.keys(this.storage); 
+    }
+}
